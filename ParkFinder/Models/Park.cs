b4aeaca267cfff01409789c4eaa43986fa9ff327
rochellev <rochellevroberts@utexas.edu>
2019-08-23 13:40:01 -0700

    add model for connecting to api

---
+++ ParkFinder/Models/Park.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ParkFinder.Models
+{
+    public class Park
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Area { get; set; }
+        public string URL { get; set; }
+
+        // API Request
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
+        public static List<Park> GetAllParksAsync()
+        {
+            var client = new RestClient("https://localhost:5001/api/");
+            var request = new RestRequest("parks", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var parkList = JsonConvert.DeserializeObject<List<Park>>(jsonResponse.ToString());
+            return parkList;
+        }
+
+
+    }
+}
